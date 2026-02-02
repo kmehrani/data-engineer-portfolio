58d057bc018afe75e931a83fda4a9050fd94039b
kmehrani <kiran.mehrani@gmail.com>
2026-02-02 14:34:16 +0400

    Logging Day 1 progress

--- practice.py
+++ practice.py
@@ -1,1 +1,4 @@
 print("I'm officially back!")
+#logging what I did today
+#I took the first step back and used hackerrank to do a couple of coding problems and GUESS WHAT! I did not forget how to code. I know it seems so minor but the anxiety of getting back into tech has slightly diminished. I am excited to move forward and build myself up again.
+print("check comments for log")
