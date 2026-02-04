b23ffbac41928130e4edbfbf9a38f1baee3a1dca
kmehrani <kiran.mehrani@gmail.com>
2026-02-04 19:32:13 +0400

    Day 3: List comprehension practice

---
+++ list_comprehensions.py
@@ -0,0 +1,7 @@
+    newList = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)
+    if i+j+k != n]
+print(newList)
+
+#logging: problem was not difficult but I did take quite a while to solve it.
+#it was a very "answer is in my face but I don't know what I'm missing"
+#I skipped over a detail in the instructions that made it harder.
